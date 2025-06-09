600dd4350f9f4a1be67214f5220f548ac3b736cd
sriom-sharan <sriomsharan.personal@gmail.com>
2025-06-09 21:15:52 +0530

    spelling mistake

--- src/components/FocusCards.tsx
+++ src/components/FocusCards.tsx
@@ -11,7 +11,7 @@ export function CardsStack() {
       src: "https://images.unsplash.com/photo-1600271772470-bd22a42787b3?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Sala behta hi jayega",
+      title: "Behta hi jaye",
       src: "https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
