5ac2de64ab3ac063e6a6dc572f850b71db2d74bd
sriom-sharan <sriomsharan.personal@gmail.com>
2025-06-10 11:25:02 +0530

    commented seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,47 +1,47 @@
-import prisma from "./client";
+// import prisma from "./client";
 
-async function main() {
-  // Create users
-  const user = await prisma.user.create({
-    data: {
-      email: "test@example.com",
-      name: "Test User",
-    },
-  });
+// async function main() {
+//   // Create users
+//   const user = await prisma.user.create({
+//     data: {
+//       email: "test@example.com",
+//       name: "Test User",
+//     },
+//   });
 
-  // Create package
-  const travelPackage = await prisma.package.create({
-    data: {
-      name: "Beach Getaway",
-      description: "Relax on sunny beaches",
-      review: "4.5",
-      imageLink: "http://example.com/beach.jpg",
-      mrp: 500.0,
-      currentPrice: 500.0, // or omit and set in middleware
-      duration: "7 days",
-      place: "Maldives",
-      startsOn: new Date("2025-07-01"),
-      endsOn: new Date("2025-07-08"),
-    },
-  });
+//   // Create package
+//   const travelPackage = await prisma.package.create({
+//     data: {
+//       name: "Beach Getaway",
+//       description: "Relax on sunny beaches",
+//       review: "4.5",
+//       imageLink: "http://example.com/beach.jpg",
+//       mrp: 500.0,
+//       currentPrice: 500.0, // or omit and set in middleware
+//       duration: "7 days",
+//       place: "Maldives",
+//       startsOn: new Date("2025-07-01"),
+//       endsOn: new Date("2025-07-08"),
+//     },
+//   });
 
-  // Create Buy relationship
-  await prisma.buys.create({
-    data: {
-      userId: user.id,
-      packageId: travelPackage.id,
-    },
-  });
-}
+//   // Create Buy relationship
+//   await prisma.buys.create({
+//     data: {
+//       userId: user.id,
+//       packageId: travelPackage.id,
+//     },
+//   });
+// }
 
-main()
-  .then(() => {
-    console.log("Seeding complete");
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .then(() => {
+//     console.log("Seeding complete");
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
